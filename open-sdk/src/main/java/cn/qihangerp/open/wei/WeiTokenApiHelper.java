cc9ef95cb22098dd3775903772cfeb095e63aeb5
Richie <280645618@qq.com>
2025-01-03 10:46:55 +0800

    添加wei接口

---
+++ open-sdk/src/main/java/cn/qihangerp/open/wei/WeiTokenApiHelper.java
@@ -0,0 +1,63 @@
+package cn.qihangerp.open.wei;
+
+
+import cn.qihangerp.open.common.ApiResultVo;
+import cn.qihangerp.open.common.ApiResultVoEnum;
+import cn.qihangerp.open.common.RemoteUtil;
+import cn.qihangerp.open.wei.service.WeiShopInfoApiService;
+import cn.qihangerp.open.wei.service.WeiTokenApiService;
+import cn.qihangerp.open.wei.vo.ShopApiResultVo;
+import cn.qihangerp.open.wei.vo.Token;
+
+public class WeiTokenApiHelper {
+    /**
+     * 获取新token
+     * @param appId
+     * @param secret
+     * @return
+     */
+    public static ApiResultVo<Token> getToken(String appId, String secret){
+        String serverUrl = "https://api.weixin.qq.com";
+        WeiTokenApiService remoting1 = RemoteUtil.Remoting(serverUrl, WeiTokenApiService.class);
+
+
+        Token token = remoting1.getToken("client_credential",appId,secret);
+        if(token.getErrcode()==null||token.getErrcode()==0) {
+            return ApiResultVo.success(token);
+        }else{
+            return ApiResultVo.error(ApiResultVoEnum.ApiException,token.getErrmsg());
+        }
+    }
+
+    /**
+     * 检查token是否过期并返回新token
+     * @param appid
+     * @param secret
+     * @param accessToken
+     * @return
+     */
+    public static ApiResultVo<Token> checkToken(String appid, String secret, String accessToken){
+        String serverUrl = "https://api.weixin.qq.com";
+        // 调用 店铺基本信息接口 验证Token
+        WeiShopInfoApiService remoting = RemoteUtil.Remoting(serverUrl, WeiShopInfoApiService.class);
+        ShopApiResultVo shopInfo = remoting.getShopInfo(accessToken);
+        if(shopInfo.getErrcode()==null||shopInfo.getErrcode()==0) {
+            Token token = new Token();
+            token.setAccess_token(accessToken);
+            token.setErrcode(0);
+            return ApiResultVo.success(token);
+        }
+        else {
+            // Token过期 (shopInfo.getErrcode() == 42001)
+//            TokenApiService remoting1 = RemoteUtil.Remoting(serverUrl, TokenApiService.class);
+//            Token token = remoting1.getToken("client_credential", appid, secret);
+//            if (token.getErrcode() == null || token.getErrcode() == 0) {
+//                return ApiResultVo.success(token);
+//            } else {
+//                return ApiResultVo.error(token.getErrcode(), token.getErrmsg());
+//            }
+            return ApiResultVo.error(shopInfo.getErrcode(), shopInfo.getErrmsg());
+        }
+    }
+}
+
---
+++ open-sdk/src/main/java/cn/qihangerp/open/wei/WeiWaybillAccountApiHelper.java
@@ -0,0 +1,62 @@
+package cn.qihangerp.open.wei;
+
+import cn.qihangerp.open.common.ApiResultVo;
+import cn.qihangerp.open.common.ApiResultVoEnum;
+import cn.qihangerp.open.common.RemoteUtil;
+import cn.qihangerp.open.wei.bo.ewaybill.AccountGetApiBo;
+import cn.qihangerp.open.wei.bo.ewaybill.DeliveryGetApiBo;
+
+import cn.qihangerp.open.wei.service.WeiWaybillApiService;
+import cn.qihangerp.open.wei.vo.Token;
+import cn.qihangerp.open.wei.vo.ewaybill.AccountVo;
+import cn.qihangerp.open.wei.vo.ewaybill.DeliveryVo;
+import org.springframework.util.StringUtils;
+
+public class WeiWaybillAccountApiHelper {
+
+    public static ApiResultVo<DeliveryVo> getDeliveryList(String appId, String appSecret, String accessToken)  {
+        String serverUrl = "https://api.weixin.qq.com";
+        ApiResultVo<Token> token = WeiTokenApiHelper.getToken(appId, appSecret);
+        if(token.getCode() != ApiResultVoEnum.SUCCESS.getIndex()) return ApiResultVo.error(ApiResultVoEnum.ApiException,"获取Token失败");
+        accessToken = token.getData().getAccess_token();
+
+
+        WeiWaybillApiService remoting = RemoteUtil.Remoting(serverUrl, WeiWaybillApiService.class);
+
+        DeliveryGetApiBo apiBo = new DeliveryGetApiBo();
+
+        apiBo.setStatus(3);
+        var res = remoting.getDeliveryList(accessToken, apiBo);
+        if(res.getErrcode() == 0){
+            // 数据
+            return ApiResultVo.success(res.getList().size(),res.getList());
+        }else {
+            return ApiResultVo.error(ApiResultVoEnum.ApiException,res.getErrmsg());
+        }
+
+
+    }
+    public static ApiResultVo<AccountVo> getAccountList(String appId, String appSecret, String accessToken)  {
+        String serverUrl = "https://api.weixin.qq.com";
+        if(!StringUtils.hasText(accessToken)) {
+            ApiResultVo<Token> token = WeiTokenApiHelper.getToken(appId, appSecret);
+            if (token.getCode() != ApiResultVoEnum.SUCCESS.getIndex())
+                return ApiResultVo.error(ApiResultVoEnum.ApiException, "获取Token失败");
+            accessToken = token.getData().getAccess_token();
+        }
+
+        WeiWaybillApiService remoting = RemoteUtil.Remoting(serverUrl, WeiWaybillApiService.class);
+
+        AccountGetApiBo apiBo = new AccountGetApiBo();
+
+        apiBo.setNeed_balance(true);
+        apiBo.setLimit(10);
+        var res = remoting.getEwaybillAccount(accessToken, apiBo);
+        if(res.getErrcode() == 0){
+            // 数据
+            return ApiResultVo.success(res.getAccount_list().size(),res.getAccount_list());
+        }else {
+            return ApiResultVo.error(ApiResultVoEnum.ApiException,res.getErrmsg());
+        }
+    }
+}
